e289e7f61fe351e12e447f78e3ab96eac645910c
HeCu22 <helgacuijpers@xs4all.nl>
2022-09-16 09:15:49 +0200

    firstsetup

--- src/App.js
+++ src/App.js
@@ -1,25 +1,66 @@
-import logo from './logo.svg';
+import React from "react";
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    function Button({children, clickHandler, isDisabled}) {
+        return (
+            <button
+                type="button"
+                disabled={isDisabled}
+                onClick={clickHandler}>
+                {children}
+            </button>);
+    }
+
+    return (
+        <>
+            <header className="top-region">
+                <div className="inner-top-region">
+                    <div className="top-navigation">
+                        <nav className="top-nav-menu">
+                            <Button
+                                children="nieuws"
+                                clickHandler={() => console.log("nieuws")}
+                                isDisabled={false}
+                            />
+
+                            <Button
+                                children="Profiel"
+                                clickHandler={() => console.log("nieuws")}
+                                isDisabled={false}
+                            />
+
+                            <Button
+                                children="Werkwijze"
+                                clickHandler={() => console.log("nieuws")}
+                                isDisabled={false}
+                            />
+
+                            <Button
+                                children="Portfolio"
+                                clickHandler={() => console.log("nieuws")}
+                                isDisabled={false}
+                            />
+
+                            <Button
+                                children="Contact"
+                                clickHandler={() => console.log("nieuws")}
+                                isDisabled={false}
+                            />
+
+                            <Button
+                                children="nieuws"
+                                clickHandler={() => console.log("inhoud")}
+                                isDisabled={false}
+                            />
+                        </nav>
+                    </div>
+                </div>
+            </header>
+            <h1>WebLeonoor</h1>
+        </>
+    );
 }
 
 export default App;
